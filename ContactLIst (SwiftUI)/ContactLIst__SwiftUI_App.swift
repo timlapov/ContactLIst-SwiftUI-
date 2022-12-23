69160e1d3ceb6e586eb19e8735c16552e9294a46
Artem LAPOV <artemlapov@me.com>
2022-12-23 13:07:48 +0100

    Initial Commit

---
+++ ContactLIst (SwiftUI)/ContactLIst__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactLIst__SwiftUI_App.swift
+//  ContactLIst (SwiftUI)
+//
+//  Created by Artem Lapov on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactLIst__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
